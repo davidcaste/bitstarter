2b036497d8fee5306a5102806937d48e281fbf45
dcastellanos <dcastellanos@indra.es>
2013-07-11 09:21:12 +0200

    Allow grader.js to take an URL as argument

--- grader.js
+++ grader.js
@@ -15,6 +15,10 @@ References:
    - https://github.com/visionmedia/commander.js
    - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
 
++ commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://visionmedia.github.io/commander.js
+
  + JSON
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
@@ -24,8 +28,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var check = require('validator').check;
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,23 +43,57 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var assertValidURL = function(url) {
+    try {
+        check(url).isUrl();
+    } catch (e) {
+        console.log("%s is not a valid URL. Exiting.", url);
+        process.exit(1);
+    }
+    return url;
+};
+
+var cheerioHtmlString = function(htmlstring) {
+    return cheerio.load(htmlstring);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(htmlstring, checksfile) {
+    $ = cheerioHtmlString(htmlstring);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
-    return out;
+    return JSON.stringify(out, null, 4);
+};
+
+var checkHtmlFile = function(htmlfile, checksfile) {
+    fs.readFile(htmlfile, function(err, data) {
+        if (err) {
+            console.log("Could not read %s. Exiting.", htmlfile);
+            process.exit(1);
+        } else {
+            var outJson = checkHtml(data, checksfile);
+            console.log(outJson);
+        }
+    });
+};
+
+var checkHtmlURL = function(htmlurl, checksfile) {
+    restler.get(program.url).on('complete', function(result) {
+        if (result instanceof Error) {
+            console.log("Could not download %s. Exiting.", program.url);
+            process.exit(1);
+        } else {
+            var outJson = checkHtml(result, checksfile);
+            console.log(outJson);
+        }
+    });
 };
 
 var clone = function(fn) {
@@ -61,16 +102,18 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file [html_file]', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url [url]', 'URL to examine', clone(assertValidURL), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if(program.url.length > 0) {
+        checkHtmlURL(program.url, program.checks);
+    } else {
+        checkHtmlFile(program.file, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
-
